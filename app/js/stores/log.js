f77e4318bc645b6f2ced587be0b849f46e583600
Kaede Takamori <etheriqa@gmail.com>
2015-06-18 21:06:00 +0900

    Refactor

--- app/js/stores/log.js
+++ app/js/stores/log.js
@@ -3,25 +3,16 @@ import Dispatcher from '../dispatcher'
 class Log {
   constructor(id, name, message) {
     this.id_ = id
-    this.date_ = new Date
     this.name_ = name
     this.message_ = message
+    this.date_ = new Date
   }
-  id() {
-    return this.id_
-  }
+  id()      { return this.id_ }
+  name()    { return this.name_ }
+  message() { return this.message_ }
   timestamp() {
     const pad = n => n < 10 ? '0' + n : '' + n
     return pad(this.date_.getHours()) + ':' + pad(this.date_.getMinutes()) + ':' + pad(this.date_.getSeconds())
-  }
-  date() {
-    return this.date_
-  }
-  name() {
-    return this.name_
-  }
-  message() {
-    return this.message_
   }
 }
 
@@ -34,16 +25,14 @@ export class SystemLog extends Log {
 export class ChatLog extends Log {}
 
 export class LogStore extends Dispatcher {
-  constructor(dispatcher) {
+  constructor(action) {
     super()
     this.id_ = 0
     this.logs_ = []
-    dispatcher.register('message', this.onMessage.bind(this))
-    dispatcher.register('chat', this.onChat.bind(this))
+    action.register('message', this.onMessage.bind(this))
+    action.register('chat', this.onChat.bind(this))
   }
-  logs() {
-    return this.logs_
-  }
+  logs() { return this.logs_ }
   onMessage(payload) {
     this.logs_.push(new SystemLog(this.id_++, payload.Message))
     this.dispatch('change')
