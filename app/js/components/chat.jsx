75fbe1b4ca11d8926a0af03c218e7e6dbd0a3cc5
Kaede Takamori <etheriqa@gmail.com>
2015-06-20 01:21:39 +0900

    Add chat commands

--- app/js/components/chat.jsx
+++ app/js/components/chat.jsx
@@ -8,8 +8,41 @@ export default class Chat extends React.Component {
   }
   handleSubmit(e) {
     e.preventDefault()
-    const message = React.findDOMNode(this).firstElementChild.value
-    this.props.action.chat(message)
+    const input = React.findDOMNode(this).firstElementChild.value.trim()
     React.findDOMNode(this).firstElementChild.value = ''
+    switch (true) {
+    // TODO check empty
+    case input.startsWith('/profile '):
+      this.props.action.profile(input.slice(9).trim())
+      break
+    case input.startsWith('/chat '):
+      this.props.action.chat(input.slice(6).trim())
+      break
+    case input.startsWith('/stage '):
+      this.props.action.stage(parseInt(input.slice(7).trim()))
+      break
+    case input.startsWith('/join '):
+      this.props.action.join(input.slice(6).trim())
+      break
+    case input === '/leave':
+      this.props.action.leave()
+      break
+    case input.startsWith('/ability'):
+      const args = input.slice(8).trim().split(' ')
+      const abilityName = args[0]
+      let objectUnitID = null
+      if (args.length > 1) {
+        objectUnitID = parseInt(args[1])
+      }
+      this.props.action.ability(abilityName, objectUnitID)
+      break
+    case input === '/interrupt':
+      this.props.action.interrupt()
+      break
+    case input[0] === '/':
+      break
+    default:
+      this.props.action.chat(input)
+    }
   }
 }
